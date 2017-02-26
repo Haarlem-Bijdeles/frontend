c87e7779e6a1fd9e8ce3aaa938abd5517198037c
Michiel Koning <mail@michielkoning.nl>
2017-02-26 16:32:23 +0100

    toegankelijkheid

--- header.php
+++ header.php
@@ -5,15 +5,17 @@
 		<a href="<?php echo home_url(); ?>" title="<?php bloginfo('name'); ?>" class="header__logo">
 			<img src="<?php bloginfo('template_url'); ?>/images/logo.png" alt="<?php bloginfo('name'); ?>" style="display: block">
 		</a>
-		<ol class="menu">
-			<li class="active">Werkwijze</li>
-			<li>Huiswerkbegeleiding</li>
-			<li>Bijles</li>
-			<li>Eindexamens</li>
-			<li>Wie zijn wij</li>
-			<li>Blog</li>
-			<li>Contact</li>
-		</ol>
+		<nav class="menu__list">
+			<ol class="menu">
+				<li class="active">Werkwijze</li>
+				<li>Huiswerkbegeleiding</li>
+				<li>Bijles</li>
+				<li>Eindexamens</li>
+				<li>Wie zijn wij</li>
+				<li>Blog</li>
+				<li>Contact</li>
+			</ol>
+		</nav>
 	</header>
 
 	<div class="content">
