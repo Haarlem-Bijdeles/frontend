b98d5688f725f5f0bd6c5b0ee7c36235baa3e08c
Michiel Koning <mail@michielkoning.nl>
2018-11-05 13:57:21 +0100

    update submodules

--- themes/haarlembijdeles/src/theme/scripts/functions.js
+++ themes/haarlembijdeles/src/theme/scripts/functions.js
@@ -1,8 +1,7 @@
 import 'defaults/scripts/ga';
 import 'defaults/scripts/accessibility';
 import 'defaults/scripts/no-js';
-import 'defaults/scripts/ga';
-import 'defaults/scripts/service-worker';
+// import 'defaults/scripts/service-worker';
 
 import './modules/fonts';
 import './modules/menu';
