f649ddcaa4fcaa850d1cbf894d5c667fa5cc690d
Michiel Koning <mail@michielkoning.nl>
2019-12-10 06:59:21 +0100

    notch

--- components/Layout/NotchWrapper.vue
+++ components/Layout/NotchWrapper.vue
@@ -1,11 +1,32 @@
 <template>
   <div class="notch-wrapper">
-    <slot />
+    <div class="layout-wrapper">
+      <slot />
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    size: {
+      type: String,
+      default: 'large',
+    },
+  },
+}
+</script>
 
 <style lang="postcss" scoped>
 .notch-wrapper {
   padding: 0 env(safe-area-inset-right) 0 env(safe-area-inset-left);
+}
+
+.layout-wrapper {
+  max-width: var(--container-width-xlg);
+  width: 100%;
+  padding-left: var(--gutter);
+  padding-right: var(--gutter);
+  margin: 0 auto;
 }
 </style>
