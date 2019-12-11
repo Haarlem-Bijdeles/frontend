b9a86fe3dd1ca5f41b43f8d76832e261dc1e3001
Michiel Koning <mail@michielkoning.nl>
2019-12-11 08:53:02 +0100

    pwa shortname

--- nuxt.config.js
+++ nuxt.config.js
@@ -70,6 +70,7 @@ export default {
     name: 'Haarlem Bijdeles',
     background_color: '#f85909',
     theme_color: '#f85909',
+    short_name: 'Haarlem Bijdeles',
   },
   i18n: {
     defaultLocale: 'nl',
