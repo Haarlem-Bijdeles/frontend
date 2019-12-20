c6808520693eecd230a9d6989a8a2498a0f970a1
Michiel Koning <michiel.koning@valtech.com>
2019-12-20 11:35:58 +0100

    ga test

--- config/googleAnalytics.js
+++ config/googleAnalytics.js
@@ -1,13 +1,12 @@
 export default {
   id: process.env.NUXT_ENV_GOOGLE_ANALYTICS_KEY,
-  set: [{ field: 'anonymizeIp', value: true }],
-  dev: false,
-  disabled: () => {
-    const doNotTrack =
-      navigator.doNotTrack || window.doNotTrack || navigator.msDoNotTrack
-    if (doNotTrack !== '1' && doNotTrack !== 'yes') {
-      return false
-    }
-    return true
-  },
+  // set: [{ field: 'anonymizeIp', value: true }],
+  // disabled: () => {
+  //   const doNotTrack =
+  //     navigator.doNotTrack || window.doNotTrack || navigator.msDoNotTrack
+  //   if (doNotTrack !== '1' && doNotTrack !== 'yes') {
+  //     return false
+  //   }
+  //   return true
+  // },
 }
