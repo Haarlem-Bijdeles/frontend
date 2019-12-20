63216c5865115edb03bf5c58798c7d79bba9dca2
Michiel Koning <michiel.koning@valtech.com>
2019-12-20 11:47:39 +0100

    donottrack

--- config/googleAnalytics.js
+++ config/googleAnalytics.js
@@ -1,12 +1,14 @@
 export default {
   id: process.env.NUXT_ENV_GOOGLE_ANALYTICS_KEY,
-  // set: [{ field: 'anonymizeIp', value: true }],
-  // disabled: () => {
-  //   const doNotTrack =
-  //     navigator.doNotTrack || window.doNotTrack || navigator.msDoNotTrack
-  //   if (doNotTrack !== '1' && doNotTrack !== 'yes') {
-  //     return false
-  //   }
-  //   return true
-  // },
+  set: [{ field: 'anonymizeIp', value: true }],
+  disabled: () => {
+    const doNotTrack =
+      window.navigator.doNotTrack ||
+      window.doNotTrack ||
+      window.navigator.msDoNotTrack
+    if (doNotTrack !== '1' && doNotTrack !== 'yes') {
+      return false
+    }
+    return true
+  },
 }
