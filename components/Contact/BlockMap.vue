9a7194c95d01329abb4a9f26b43445c0f7147fc3
Michiel Koning <michiel.koning@valtech.com>
2021-09-10 20:26:13 +0200

    map

--- components/Contact/BlockMap.vue
+++ components/Contact/BlockMap.vue
@@ -67,18 +67,17 @@ export default {
       }
     },
     boundMap() {
-      this.$refs.refMap.$mapPromise.then((map) => {
-        const bounds = new window.google.maps.LatLngBounds()
-        this.offices.forEach((location) => {
-          const position = new window.google.maps.LatLng(
-            location.latitude,
-            location.longitude,
-          )
-          bounds.extend(position)
-        })
-
-        map.fitBounds(bounds)
-      })
+      // this.$refs.refMap.$mapPromise.then((map) => {
+      //   const bounds = new window.google.maps.LatLngBounds()
+      //   this.offices.forEach((location) => {
+      //     const position = new window.google.maps.LatLng(
+      //       location.latitude,
+      //       location.longitude,
+      //     )
+      //     bounds.extend(position)
+      //   })
+      //   map.fitBounds(bounds)
+      // })
     },
     toggleInfoWindow(marker, ID) {
       this.infoWindowPos = this.getPosition(marker)
